d11c5a7b5e35962ecef5471e9f49e60e0a50bf44
Attila <76530842+attilafulop@users.noreply.github.com>
2021-11-16 18:27:14 +0000

    Create mapping.sol

---
+++ mapping.sol
@@ -0,0 +1,20 @@
+pragma solidity >= 0.7.0 < 0.9.0;
+
+contract exerciseMapping {
+    
+    struct Movie {
+        
+        string name;
+        string director;
+        
+    }
+    
+    mapping(uint => Movie) public movies;
+    
+    function addMovie(uint id, string memory name, string memory director) public {
+        
+        movies[id] = Movie(title, director);
+        
+    }
+    
+}
